a58b0f424b5f46a5bf9b51e761c6597435fbdabb
Filip Jurcak <jurcak.filip@gmail.com>
2017-05-31 16:17:40 +0200

    Reverse discussion list in chronological order (#9)

---
+++ popup.js
@@ -0,0 +1,48 @@
+// const utils = require("./utils")
+
+// TODO: Move to utils and add build step
+function rotateDiscussion(id) {
+    const elem = document.getElementById(id);
+    elem.style.display = "flex";
+    elem.style.flexDirection = "column-reverse";
+}
+
+const ROUTES = {
+  ISSUE: "ISSUE",
+  ISSUES: "ISSUES",
+  MR: "MR",
+  MRS: "MRS"
+};
+
+const pathnameToRoute = input => {
+  // /kiwi/frontend/merge_requests/1800
+  const parts = input.split("/");
+  const group = parts[1];
+  const project = parts[2];
+  const module = parts[3];
+  const id = parts[4];
+
+  switch (module) {
+    case "issues":
+      return id ? ROUTES.ISSUE : ROUTES.ISSUES;
+    case "merge_requests":
+      return id ? ROUTES.MR : ROUTES.MRS;
+  }
+};
+
+function main() {
+  const route = pathnameToRoute(location.pathname);
+  switch (route) {
+    case ROUTES.MR:
+    case ROUTES.ISSUE:
+      rotateDiscussion("notes-list");
+  }
+}
+
+main();
+
+// Exports for tests
+module.exports = {
+  ROUTES,
+  pathnameToRoute
+};
