561ca2a08489919059248fdcc05180791928502b
Strajk <strajk@me.com>
2017-10-16 20:13:05 +0200

    Fix #45

--- src/libs/transformations/expandAll.js
+++ src/libs/transformations/expandAll.js
@@ -1,7 +1,8 @@
 export default () => {
   const a = location.pathname.split("/");
   if (a[5] === "diffs" && a[3] === "merge_requests") {
-    const url = "?expand_all_diffs=1";
-    if (!window.location.href.includes(url)) window.location.href += url;
+    const postfix = "expand_all_diffs=1";
+    const separator = window.location.href.includes("?") ? "&" : "?";
+    if (!window.location.href.includes(postfix)) window.location.href += `${separator}${postfix}`;
   }
 };
