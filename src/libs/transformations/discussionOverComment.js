37c3fed5c55c48384a2f3107ff725e6595c83bcd
Strajk <strajk@me.com>
2018-12-07 10:29:26 +0100

    Fix discussion over comment

--- src/libs/transformations/discussionOverComment.js
+++ src/libs/transformations/discussionOverComment.js
@@ -1,5 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
 export const agent = () => {
-  const el = document.getElementById("discussion");
-  el.click();
+  const timer = setInterval(() => {
+    const el = document.querySelector(".js-main-target-form .qa-discussion-option");
+    if (el) {
+      clearInterval(timer)
+      console.log("CLICKING");
+      el.click();
+    }
+  }, 1000);
 };
