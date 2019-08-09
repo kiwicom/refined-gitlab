109a36806942a8dbc3a17af4189933ffa33faae4
Federico Brigante <github@bfred.it>
2019-08-09 19:09:17 +0700

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,10 +1,9 @@
-import injectContentScripts from "webext-dynamic-content-scripts";
-import DPT from "webext-domain-permission-toggle";
+import 'webext-dynamic-content-scripts';
+import addDomainPermissionToggle from 'webext-domain-permission-toggle';
 import * as storage from "./options/storage";
 import insight from "./insight";
 
-DPT.addContextMenu();
-injectContentScripts();
+addDomainPermissionToggle();
 
 storage.load().then(() => {
   insight("UA-102570755-1", storage.get("clientId"), storage.get("doTrack"));
